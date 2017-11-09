e5941a31b7401580eac4a6853f31358554efbc05
Tomas Korcak <korczis@gmail.com>
2017-11-09 18:10:33 +0100

    Implemented toposort

--- main.rb
+++ main.rb
@@ -2,7 +2,10 @@ require 'fileutils'
 require 'json'
 require 'gooddata'
 
-PID = 'la84vcyhrq8jwbu4wpipw66q2sqeb923'
+USERNAME = ENV['GDC_USERNAME'] || 'bear@gooddata.com'
+PASSWORD = ENV['GDC_PASSWORD'] || ''
+SERVER = ENV['GDC_SERVER'] || 'https://instance-develop-45.dev.intgdc.com'
+PID = ENV['GDC_PID'] || 'l739tknzsa2b6mp9m49e1s6m1s8jdmhi'
 
 def fetch(client, link)
   puts "Fetching #{link}"
@@ -12,9 +15,7 @@ def fetch(client, link)
   items = res['objects']['items']
 
   next_link = res['objects']['paging']['next']
-  if next_link
-   items += fetch(client, next_link)
-  end
+  items += fetch(client, next_link + '&include=predecessors') if next_link
 
   items
 end
@@ -23,7 +24,7 @@ def main
   data_dir = File.join(File.dirname(__FILE__), 'data', PID)
   FileUtils.mkdir_p(data_dir)
 
-  client = GoodData.connect
+  client = GoodData.connect(USERNAME, PASSWORD, server: SERVER, verify_ssl: false)
 
   response = client.get("/gdc/md/#{PID}/objects/query")
 
@@ -32,7 +33,7 @@ def main
   data = links.map do |link|
     begin
       category = link['category']
-      items = fetch(client,link['link'].gsub('limit=5', 'limit=50'))
+      items = fetch(client, link['link'].gsub('limit=5', 'limit=50') + '&include=predecessors')
 
       category_dir = File.join(data_dir, category)
       FileUtils.mkdir_p(category_dir)
@@ -42,7 +43,7 @@ def main
         item_path = File.join(category_dir, "#{identifier}.json")
 
         puts "Writing #{item_path}"
-        File.open(item_path,'w') do |f|
+        File.open(item_path, 'w') do |f|
           f.write(JSON.pretty_generate(item))
         end
       end
@@ -57,9 +58,58 @@ def main
   # Remove nil values
   data.compact!
 
-  # TODO: Fix order here!
+  objects = data.map(&:last)
+  objects.flatten!
+  # pp objects
+
+  lookup = {}
+  objects.each do |obj|
+    root = obj.keys.first
+    obj_uri = obj[root]['meta']['uri']
+    lookup[obj_uri] = obj
+  end
+
+  sorted = []
+  processed = Set.new
+  uris = lookup.keys
+  while uri = uris.shift do
+    toposort(processed, sorted, uri, lookup)
+  end
+
+  sorted.uniq!
+
+  sorted.each do |uri|
+    obj = lookup[uri]
+    key = obj.keys.first
+    raw = obj[key]
+
+    puts "#{key}: #{raw['meta']['identifier']} - #{raw['meta']['uri']}"
+  end
 end
 
-if __FILE__ == $PROGRAM_NAME
-  main
+def toposort(processed, sorted, uri, lookup)
+  obj = lookup[uri]
+
+  if processed.include?(uri)
+    return
+  end
+
+  if obj.nil?
+    puts "Unable to find object #{uri}"
+    return
+  end
+
+  root = obj.keys.first
+  raw = obj[root]
+
+  predecessors = raw['links']['predecessors'] || []
+  predecessors.each do |predecessor|
+    toposort(processed, sorted, predecessor, lookup)
+  end
+
+  processed.add(uri)
+
+  sorted.push(uri)
 end
+
+main if __FILE__ == $PROGRAM_NAME
